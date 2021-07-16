d1bebefab259995607fa3d4a6bf19723c578b27e
fgames-dev <70545380+fgames-dev@users.noreply.github.com>
2021-07-16 13:18:55 -0500

    Add sounds
    You can now use the playSound() function with 1 parameter: src (path to audio file). Cleaned up the code.

--- instant.js
+++ instant.js
@@ -24,8 +24,6 @@ window.addEventListener("touchmove", function(e){
     mouseY = e.clientY - parseInt(window.getComputedStyle(c, null).paddingTop) * scaleY;
 });
 
-//swal("InstantJS", "Made with InstantJS", "info");
-
 function extend(object, code){
     object.extension = code;
 }
@@ -143,6 +141,7 @@ var shader = {
 
 function conLog(text){
     //document.getElementById("cons").value += `\n ${text}`;
+    //Used to be a custom console
 }
 
 function condition(con, code){
@@ -163,7 +162,7 @@ function sendMultiplayer(data, where, back){
     let wehere = where;
     let what = data;
     let bk = back;
-    window.location = "write.php?where=" + where + "&what=" + what + "back=" + back;
+    window.location = "write.php?where=" + where + "&what=" + what + "&back=" + back;
 }
 
 function Text(x, y, size, font, text, color){
@@ -202,75 +201,6 @@ function screenScale(){
     }
 }
 
-//Testing only--DO NOT USE!
-function render(sprite, xe, ye, w, h){
-    let y = ye;
-    let x = xe;
-    let ones = [];
-    ctx.fillStyle = sprite[0];
-    for(i = 1; i < sprite.length; i++){
-        if(i == 8 || i == 16 || i == 24|| i == 32 || i == 40 || i == 48 || i == 56){
-                x = xe;
-                y += (h/8)
-        }
-        if(sprite[i] == 1){
-            ones.push(i);
-            //console.log(y);
-            //console.log("x" + x);
-            //console.log(ones);
-            
-            /*if(ones[ones.length-1] - ones[ones.length-2] >= 2){
-                x = xe;
-                y += (h/8)
-                console.log(`first ${ones[ones.length-2]}, second ${ones[ones.length-1]}`);
-            }*/
-            if(i <= 8){
-                //x = 0;
-                x += w/8;
-                ctx.fillRect(x, y, w/8, h/8);
-            }
-            if(i > 8 && i <= 16){
-                //x = 0;
-                x += w/8;
-                ctx.fillRect(x, y, w/8, h/8);
-            }
-            if(i > 16 && i <= 24){
-                //x = 0;
-                x += w/8;
-                ctx.fillRect(x, y, w/8, h/8);
-            }
-            if(i > 24 && i <= 32){
-                //x = 0;
-                x += w/8;
-                ctx.fillRect(x, y, w/8, h/8);
-            }
-            if(i > 32 && i <= 40){
-                //x = 0;
-                x += w/8;
-                ctx.fillRect(x, y, w/8, h/8);
-            }
-            if(i > 40 && i <= 48){
-                //x = 0;
-                x += w/8;
-                ctx.fillRect(x, y, w/8, h/8);
-            }
-            if(i > 48 && i <= 56){
-                //x = 0;
-                x += w/8;
-                ctx.fillRect(x, y, w/8, h/8);
-            }
-            if(i > 56 && i <= 64){
-                //x = 0;
-                x += w/8;
-                ctx.fillRect(x, y, w/8, h/8);
-            }
-        } else{
-            x += w/8;
-        }
-    }
-    conLog("render");
-}
-
 function pixelPerfect(color, numberx, numbery){
     ctx.fillStyle = color;
     ctx.fillRect(numberx * 10, numbery * 10, 10, 10);
@@ -281,19 +211,6 @@ function handleInputDown(input, result){
     window.addEventListener(`keydown`, function(e){
         if(e.key.toLowerCase() == input){
             result();
-            conLog("Key input!");
-        }
-    });
-}
-
-function handle2InputsDown(inputA, inputB, resultA, resultB){
-    window.addEventListener(`keydown`, function(e){
-        if(e.key.toLowerCase() == inputA){
-            resultA();
-            conLog("Key input!");
-        }
-        if(e.key.toLowerCase() == inputB){
-            resultB();
             conLog("Key input!");
         }
     });
@@ -344,98 +261,10 @@ function blockTilemap(map, size, sprite){
     return result;
 }
 
-function TilemapUnstable(map, key, sprites, w, h){
-    this.map = map;
-    this.key = key;
-    this.x = 0;
-    this.y = 0;
-    this.w = w;
-    this.h = h;
-    this.sprites = sprites;
-    this.generatedMap = [];
-    this.init = function(){
-        for(i = 0; i < this.map.length; i++){
-            for(e = 0; e < this.map[i].length; e++){
-                this.generatedMap.push(this.key[this.map[i][e]]);
-                this.generatedMap[this.generatedMap.length - 1].sprite = this.sprites[this.map[i][e]];
-                this.generatedMap[this.generatedMap.length - 1].x = this.x;
-                this.generatedMap[this.generatedMap.length - 1].y = this.y;
-                this.generatedMap[this.generatedMap.length - 1].w = this.w;
-                this.generatedMap[this.generatedMap.length - 1].h = this.h;
-                this.x += this.w;
-            }
-            this.y += this.h;
-        }
-        for(a = 0; a < this.generatedMap.length; a++){
-            this.generatedMap[a].render();
-        }
-    }
-    this.render = function(){
-        for(a = 0; a < this.generatedMap.length; a++){
-            this.generatedMap[a].render();
-        }
-    }
-}
-
 function imgDraw(img, x, y, w, h){
     let imagen = new Image();
     imagen.src = img;
     ctx.drawImage(imagen, x, y, w, h);
-}
-
-function Particles(num, x, y, playing, size, xv, yv, color, lf){
-    this.num = num;
-    //this.type = type;
-    this.x = x;
-    //this.fr = fr;
-    this.y = y;
-    this.playing = playing;
-    this.size = size;
-    //this.xr = xr;
-    //this.yr = yr;
-    this.xv = xv;
-    this.yv = yv;
-    //this.xvc = xvc;
-    //this.yvc = yvc;
-    //this.sc = sc;
-    this.color = color;
-    this.lf = lf;
-    this.nlf = lf;
-    var xn = this.x;
-    var yn = this.y;
-    var sn = this.size;
-    var vx = this.xv;
-    var vy = this.yv;
-    var xvn = vx;
-    var yvn = vy;
-    this.init = function(){
-        if(this.playing && lf >= 0){
-            //var timnim = 0;
-            for(i = 0; i < this.num; i++){
-                //timnim++;
-                ctx.fillStyle = this.color;
-                this.draw(xn, yn, sn, sn);
-            }
-            this.lf--;
-        } else{
-            this.lf = this.nlf;
-        }
-    };
-    this.draw = function(x, y, w, h){
-        ctx.fillRect(x, y, w, h);
-    }
-    this.update = function(){
-        if(this.lf > 0){
-            xn += xvn/10;
-            yn += yvn/10;
-            vy += this.yvc/10;
-            vx += this.xvc/10;
-            sn += this.sc/10;
-            ctx.fillStyle = this.color;
-            ctx.fillRect(xn, yn, sn, sn);
-            this.lf--;
-        }
-    }
 }
 
 function Scene(config){
@@ -559,6 +388,7 @@ function Block(sprite, x, y, w, h){
     gameBlocks.push(this);
 }
 
+//Testing only
 function PhysicsBody(sprite, x, y, w, h, camx, camy, grav, mass, mode){
     this.x = x;
     this.sx = x;
@@ -839,6 +669,7 @@ function Character(sprite, x, y, w, h, camx, camy){
     conLog("Character class!");
 }
 
+//Testing only
 function raycast(x, y, tx, ty, obs){
     let cast = new Character("https://future-games.site/tj2cover.png", x, y, 1, 1);
     let collided = false;
@@ -869,6 +700,13 @@ function Sprite(src, x, y, w, h){
         ctx.drawImage(this.img, this.x, this.y, this.w, this.h);
     }
 }
+
+function playSound(src){
+    let sound = new Audio();
+    sound.src = src;
+    sound.play();
+}
+
 
 
 
